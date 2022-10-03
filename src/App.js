5e6a44381336cd3bca31e8efc221e78d7b3830d2
Francis Guido <fjguido97@gmail.com>
2022-10-03 15:47:42 -0400

    deleted useless files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="bg-purple-500">
+    <div>
       <h1 className='text-xl'>Hello World</h1>
-      <button className='btn'>Click</button>
-     
     </div>
   );
 }
