f554760622401547d2d95ceabff114fa08436764
Francis Guido <fjguido97@gmail.com>
2022-10-14 16:02:23 -0400

    was able to display users

--- src/components/Users/UserResults.jsx
+++ src/components/Users/UserResults.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 function UserResults() {
+
+    const[users, setUsers] = useState([])
+    const[loading, setLoading] = useState(true)
 
     useEffect(() => {
         fetchUsers()
-
     }, [])
 
     const fetchUsers = async () => {
@@ -15,10 +17,17 @@ function UserResults() {
             }
         })
         const data = await response.json()
-        console.log(data)
+
+       setUsers(data)
+       setLoading(false)
     }
+
   return (
-    <div>UserResults</div>
+    <div className='grid grid-cols-1 gap-8 xl:gril-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((users) => (
+           <h3>{users.login}</h3> 
+        ))}
+    </div>
   )
 }
 
